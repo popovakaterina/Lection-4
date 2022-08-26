50e2b17237f81ba11d245e6e4e4cf9a2990db86b
Popova Ekaterina <popova_katerina97@mail.ru>
2022-08-26 17:31:15 +0500

    Объявление и вывод пустого массива

---
+++ 001_example/Program.cs
@@ -0,0 +1,21 @@
+﻿string [,]table = new string [2,5];
+table [1,2] = "word";
+for (int rows = 0; rows < 2; rows ++) // распечатать массив
+{
+    for (int columns = 0; columns <5; columns++)
+    {
+        Console.Write($"-{table[rows,columns]}-"); // вывод массива
+    }
+Console.WriteLine();
+}
+Console.WriteLine("Следующий массив");
+
+int[,] matrix = new int [3,4];
+for (int rows = 0; rows < 3; rows ++) // распечатать массив
+{
+    for (int columns = 0; columns <4; columns++)
+    {
+        Console.Write($"{matrix[rows,columns]} "); // вывод массива
+    }
+Console.WriteLine();
+}

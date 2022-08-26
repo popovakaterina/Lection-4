e6b299b0f60a5a394c2a08aa3b01ec9f360de282
Popova Ekaterina <popova_katerina97@mail.ru>
2022-08-26 22:06:00 +0500

    Вывод матрицы и заполнение ее данными

---
+++ 002_example/Program.cs
@@ -0,0 +1,31 @@
+﻿// Печать матрицы и заполнение ее числами
+void PrintArray(int [,] matr) // Метод распечатки массива
+{
+    for (int i = 0; i < matr.GetLength(0); i++) // распечатать массив
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} "); // вывод массива
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void FillArraay (int [,] matr) // Заполение массива случайными числамии
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random ().Next(1,10);
+
+        }
+    }
+}
+int [,] matrix = new int [3,4];
+
+PrintArray(matrix);
+FillArraay (matrix);
+Console.WriteLine();
+PrintArray(matrix);
